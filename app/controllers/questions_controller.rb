2f4693cc88b2171d36ac10d715cd24657c1c069f
Laura Decroix <laura.decroix@ece-france.com>
2018-11-05 15:20:18 +0100

    commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work!"
+     return @answer = "Great"
+
+    elsif @question.include?("?")
+     return @answer = "Silly question, get dressed and go to work!"
+
+    elsif @question != "I am going to work!"
+    return @answer = "I don't care, get dressed and go to work!"
+  end
+  end
+end
+
+
+
+
